5da0c0416ca0caec28e1821400f65c3b5f71c6f0
Jayashree-Kulothungan <52423488+Jayashree-Kulothungan@users.noreply.github.com>
2020-09-16 18:02:51 +0530

    commit-1

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,95 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../pages/Home'
+import store from '../store/index'
 
 Vue.use(VueRouter)
-
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
-})
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+  },
+  {
+    path: '/Clinics',
+    name: 'Clinics',
+    component: () => import('../pages/Clinics.vue')
+  },
+  {
+    path: '/Training',
+    name: 'Training',
+    component: () => import('../pages/Training.vue')
+  },
+  {
+    path: '/Buy-Sell',
+    name: 'BuySell',
+    component: () => import('../pages/BuySell.vue')
+  },
+  {
+    path: '/Grooming',
+    name: 'Grooming',
+    component: () => import('../pages/Grooming.vue')
+  },
+  {
+    path: '/DayCare',
+    name: 'DayCare',
+    component: () => import('../pages/DayCare.vue')
+  },
+  {
+      path: '/DogWalker',
+      name: 'DogWalker',
+      component: () => import('../pages/DogWalker.vue')
+  }, 
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: () => import('../pages/Profile.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+      path: '/login',
+      name: 'login',
+      component: () => import('../pages/Login/Login.vue'),
+      meta: {
+        requiresGuest: true
+      }
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../pages/Login/Register.vue'),
+      meta: {
+        requiresGuest: true
+      }
+    }
+  ]
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!store.getters.isLoggedIn) {
+      // Redirect to the Login Page
+      next('/login');
+    } else {
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    if (store.getters.isLoggedIn) {
+      // Redirect to the Profile Page
+      next('/profile');
+    } else {
+      next();
+    }
+  } else {
+    next()
+  }
+});
+
+
+export default router;
