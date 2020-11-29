02ca9669bc3201a98abc8fedcbb0137bc763dc1c
Jayashree-Kulothungan <52423488+Jayashree-Kulothungan@users.noreply.github.com>
2020-11-29 22:06:39 +0530

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,20 +2,26 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import Auth from '../Warehouse/Auth';
+import Cart from '../Warehouse/Cart';
+import DayCare from '../Warehouse/DayCare';
+import Services from '../Warehouse/Services';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    Auth
+    Auth,
+    Services,
+    Cart,
+    DayCare
   },
   state: {
 
-  },
+  }, 
   mutations: {
 
   },
   actions: {
 
-  }
+  },
 })
