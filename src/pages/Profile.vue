8167391941bddeb57f17c028f4b7ba9340d96bd2
Jayashree-Kulothungan <52423488+Jayashree-Kulothungan@users.noreply.github.com>
2020-11-29 22:05:19 +0530

    Update Profile.vue

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,336 +1,298 @@
 <template>
- <div>
-    <div class= "container">
-      <!--Account details -->
-      <div >
-        <p id="title">My Account</p>
-        <hr id="hr">
-        <div class="card">
-          <table>
-            <tr><th>Name  :</th><td>{{ user._id }}</td></tr>
-            <tr><th>Email  :</th><td>{{ user.email}}</td></tr>
-            <tr><th>Mobile  :</th><td>{{ user.mobile}}</td></tr>
-            <tr><th>Address  :</th><td>{{ user.address}}</td></tr>
-          </table>
-        </div>
-        <button id="regser" >Register Service</button>
-      </div>
-    </div>
- </div>
+<div>
+  <div class="profile">
+  <v-card>
+    <v-tabs vertical>
+      <v-tab id="my-acc" selected @click="showAccount" >  My Account  </v-tab>
+      <p id="services"  @click="showServices">Services<b-icon icon="chevron-down" id="arrow" scale="1"></b-icon></p>
+        <v-tab id="day-care" v-if="showStabs" @click="showDaycare">  Day Care  </v-tab>
+        <v-tab id="grooming" v-if="showStabs" @click="showGrooming">  Grooming  </v-tab>
+        <v-tab id="dog-walker" v-show="showStabs" @click="showDogwalker"> Dog Walker </v-tab>
+        <v-tab id="trainer" v-show="showStabs" @click="showTrainer"> Trainer </v-tab>
+        <v-tab id="clinics" v-show="showStabs" @click="showClinics">  Clinics  </v-tab>
+        <v-tab id="breeding" v-show="showStabs" @click="showBreeding">  Breeding </v-tab> 
+        <v-tab id="sell" v-show="showStabs">Sell</v-tab> 
+    </v-tabs>
+  </v-card>
+    <my-account v-if="myaccount"></my-account> 
+    <day-care v-if="daycare"></day-care>
+    <grooming v-if="grooming"></grooming>
+    <dog-walker v-if="dogwalker"></dog-walker>
+    <trainer v-if="trainer"></trainer>
+    <clinics v-if="clinics"></clinics>
+    <breeding v-if="breeding"></breeding>
+
+    
+</div>
+</div>
 </template>
+
 <script>
+import myAccount from '../components/services/account'
+import dayCare from '../components/services/daycare'
+import Grooming from '../components/services/grooming'
+import dogWalker from '../components/services/dogwalker'
+import Trainer from '../components/services/training'
+import Clinics from '../components/services/clinics'
+import Breeding from '../components/services/breeding'
 import { mapActions, mapGetters } from "vuex";
 export default {
    data() {
     return {
       myaccount: true,
+      showStabs: false,
+      daycare:false,
+      grooming:false,
+      dogwalker:false,
+      trainer:false,
+      clinics:false,
+      breeding:false,
       regService :'',
       name : '',
       
     }
   },
-  beforeCreate: function() {
-        document.body.className = 'Profile';
-    },
   computed: mapGetters(["user"]), 
   methods: {
     ...mapActions(["getProfile"]),
+    showAccount(){
+      this.myaccount=true;
+      this.daycare=false;
+      this.grooming=false;
+      this.dogwalker=false;
+      this.trainer=false;
+      this.clinics=false;
+      this.breeding=false;
+    },
+    showServices(){
+      this.showStabs=!this.showStabs;
+      this.myaccount=true;
+      this.daycare=false;
+      this.grooming=false;
+      this.dogwalker=false;
+      this.trainer=false;
+      this.clinics=false;
+      this.breeding=false;
+    },
+    showDaycare(){
+      this.myaccount=false;
+      this.daycare=true;
+      this.grooming=false;
+      this.dogwalker=false;
+      this.trainer=false;
+      this.clinics=false;
+      this.breeding=false;
+    },
+    showGrooming(){
+      this.myaccount=false;
+      this.daycare=false;
+      this.grooming=true;
+      this.dogwalker=false;
+      this.trainer=false;
+      this.clinics=false;
+      this.breeding=false;
+    },
+    showDogwalker(){
+      this.myaccount=false;
+      this.daycare=false;
+      this.grooming=false;
+      this.dogwalker=true;
+      this.trainer=false;
+      this.clinics=false;
+      this.breeding=false;
+    },
+    showTrainer(){
+      this.myaccount=false;
+      this.daycare=false;
+      this.grooming=false;
+      this.dogwalker=false;
+      this.trainer=true;
+      this.clinics=false;
+      this.breeding=false;
+    },
+    showClinics(){
+      this.myaccount=false;
+      this.daycare=false;
+      this.grooming=false;
+      this.dogwalker=false;
+      this.trainer=false;
+      this.clinics=true;
+      this.breeding=false;
+    },
+    showBreeding(){
+      this.myaccount=false;
+      this.daycare=false;
+      this.grooming=false;
+      this.dogwalker=false;
+      this.trainer=false;
+      this.clinics=false;
+      this.breeding=true;
+    }
   },
   created() {
     this.getProfile();
-    //this.name = this.user._id
   },
+  components:{
+    myAccount,
+    dayCare,
+    Grooming,
+    dogWalker,
+    Trainer,
+    Clinics,
+    Breeding
+  },
+  
 };
 </script>
 
 <style scoped>
-#services{
-  position: absolute;
-  top:190px;
-  left:500px;
-  width: 300px;
-  height: 45px;
-  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border: 1px solid #DCDCDC;
-  border-radius: 3px;
-  opacity: 1;
-  text-align: left;
-  font: normal normal 300 20px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1;
-}
-#ser{
-  position: absolute;
-  top:150px;
-  left:500px;
-  text-align: left;
-  font: normal normal 300 20px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1; 
-}
-#zip{
-  position: absolute;
-  top:260px;
-  left:30px;
-  text-align: left;
-  font: normal normal 300 20px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1; 
-}
-#zipcode {
-  position: absolute;
-  top:300px;
-  left:30px;
-  width: 300px;
-  height: 45px;
-  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border: 1px solid #DCDCDC;
-  border-radius: 3px;
-  opacity: 1;
-  text-align: center;
-  font: normal normal 300 20px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1;
-}
-#norg{
-  position: absolute;
-  top:150px;
-  left:30px;
-  text-align: left;
-  font: normal normal 300 20px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1;
-}
-#name {
-  position: absolute;
-  top:190px;
-  left:30px;
-  width: 300px;
-  height: 45px;
-  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border: 1px solid #DCDCDC;
-  border-radius: 3px;
-  opacity: 1;
-  text-align: left;
-  font: normal normal 300 20px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1;
-}
-.tab{
-  position: absolute;
-  top: 130px;
-  left: 5px;
-  width: 250px;
-  height: 550px;
-  background: var(--unnamed-color-ffffff) 0% 0% no-repeat padding-box;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  opacity: 1;
-  border-right: #DCDCDC solid 10px;
-  
-}
-#myacc{
-  position: absolute;
-  top: 50px;
-  left: 0px;
-  width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#select{
-  position: absolute;
-  top: 100px;
-  left: 0px;
-  width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#dc{
-  position: absolute;
-  top: 150px;
-  left: 0px;
-  width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#gr{
-  position: absolute;
-  top: 200px;
-  left: 0px;
-  width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#dw{
-  position: absolute;
-  top: 250px;
-  left: 0px;
- width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#tr{
-  position: absolute;
-  top: 300px;
-  left: 0px;
-  width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#cl{
-  position: absolute;
-  top: 350px;
-  left: 0px;
-  width: 235px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border-radius: 3px;
-  opacity: 1;
-  border-top: #707070 2px;
-  border-bottom: #707070 2px;
-  border-left: none;
-  border-right:none;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-align:left;
-  text-indent: 30px;
-}
-#regser{
-  position: absolute;
-  top: 450px;
-  left: 450px;
-  width: 200px;
-  height: 40px;
-  border: none;
-  background: var(--unnamed-color-ff5e41) 0% 0% no-repeat padding-box;
-  background: #FF5E41 0% 0% no-repeat padding-box;
-  border-radius: 32px;
-  opacity: 1;
-}
-.container {
-  position: absolute;
-  top:130px;
-  left:320px;
-  width: 950px;
-  height: 550px;
-  border-style: solid;
-  border-color:#D3D3D3; 
-  border-width: 2px; 
-}
-#title{
-  position: absolute;
-  top:50px;
-  left:50px;
-  text-align: left;
-  font: normal normal normal 24px/35px Poppins;
-  letter-spacing: 0px;
-  color: #2E67CE;
-  opacity: 1;
-}
-#hr{
-  position: relative;
-  top:90px;
-  border-width: 3px;
-}
-.card{
-  border: none;
-  position: relative;
-  top:90px;
-  width:400px;
-  left:50px;
-  text-align: left;
-  font: normal normal 300 18px/35px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  opacity: 1;
-}
-th{
-  border:none;
-  width:80px
-}
-tr{
-  height:50px;
-}
-.list-group-item{
-  border: none
-}
-*:focus {
-    outline: 0 !important;
-}
 
+  *:focus {
+      outline: 0 !important;
+  }
+  .profile{
+    position: absolute;
+    top:100px;
+    left:0px;
+  }
+  #my-acc{
+    position: absolute;
+    top:0px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070!important;
+    border:none;
+  }
+  #services{
+    position: absolute;
+    top:70px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070!important;
+    border:none;
+  }
+  #arrow{
+    position: absolute;
+    left:90%;
+    
+  }
+  #day-care{
+    position: absolute;
+    top:140px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
+  #grooming{
+    position: absolute;
+    top:210px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
+  #dog-walker{
+    position: absolute;
+    top:280px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
+  #trainer{
+    position: absolute;
+    top:350px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
+  #clinics{
+    position: absolute;
+    top:420px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
+  #breeding{
+    position: absolute;
+    top:490px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
+  #sell{
+    position: absolute;
+    top:560px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+  }
 </style>
