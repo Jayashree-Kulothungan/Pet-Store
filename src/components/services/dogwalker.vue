77365c322efe707507672b5c9cb8d63113548a29
Jayashree-Kulothungan <52423488+Jayashree-Kulothungan@users.noreply.github.com>
2020-12-21 08:34:28 +0530

    Update dogwalker.vue

--- src/components/services/dogwalker.vue
+++ src/components/services/dogwalker.vue
@@ -32,11 +32,26 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
   data (){
     return {
       availableTime : false,
-      price : false
+      price : false,
+      data : {
+        dogwalker : {
+          availabletime : {
+            weekdays : '',
+            weekends : ''
+        },
+        description :'',
+        price : {
+            weekdays : '',
+            weekends : ''
+        },
+        additionalServices : '',
+        }
+      }
     }
   },
   methods : {
@@ -47,7 +62,32 @@ export default {
     showPrice() {
       this.price = !this.price
       this.availableTime=false
+    },
+     ...mapActions(["updateServices"]),
+    handleSubmit(){
+      //console.log(JSON.parse(JSON.stringify(this.data)))
+      this.updateServices(JSON.parse(JSON.stringify(this.data)))
     }
+  },
+  computed : {
+    ...mapGetters(["services"]),
+    dayCare () {
+      if (!this.services[0]) {
+        return {
+          availabletime : {
+            weekdays : '',
+            weekends : ''
+          },
+          description :'',
+          price : {
+              weekdays : '',
+              weekends : ''
+          },
+          additionalServices : '',
+          }  
+       }
+      return this.services[0].dogwalker
+    },
   }
 }
 </script>
