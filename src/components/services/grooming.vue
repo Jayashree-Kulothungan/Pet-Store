8fcab5e5d632c788daaae481f3738ec0a7ec7bbe
Jayashree-Kulothungan <52423488+Jayashree-Kulothungan@users.noreply.github.com>
2020-12-21 08:34:58 +0530

    Update grooming.vue

--- src/components/services/grooming.vue
+++ src/components/services/grooming.vue
@@ -4,16 +4,23 @@
             <p id="title">Grooming</p>
             <hr id="hr">
             <div class="card">
-                <form>
-                  <input type="text" id="time" placeholder="Open Time">
-                  <input type="text" id="description" placeholder="Description">
+                <form @submit.prevent="handleSubmit" >
+                  <!-- Timings -->
+                  <p id="time" @click= "showTime">Opens At</p>
+                  <div class="time" v-if="time">
+                    <p id="weekdays"> Weekdays </p> <input type="time" id="weekdaytime" placeholder="Enter time" >
+                    <p id="weekends"> Weekends </p> <input type="time" id="weekendtime" placeholder="Enter time" >          
+                  </div>
+                  <!-- Description -->
+                  <input type="text" id="description" placeholder="Description"  > 
+                  <!-- Image -->
                   <input type="text" id="photo" placeholder="Upload Photos" disabled>
-                  <input type="file" id="photofile">
-                  <input type="text" id="price" placeholder="Price">
+                    <input type="file" id="photofile">
+                  <!-- Other Specifications -->
+                    <input type="text" id="other" placeholder="Other Specifications" >
+                  <!--Services -->
                   <p id="service" @click="showServices">Services </p>
-                  <input type="text" id="other" placeholder="Other Specifications">
-
-                  <div class="services" v-if="services">
+                  <div class="services" v-if="service">
                     <p id="haircut"><input type = "checkbox" value="haircut"><label>Haircut</label></p>
                       <p id="haircut-price">Price:</p><input type="text" id="haircut-pr">
                     <p id="bodywash"><input type = "checkbox" value="body wash"><label>Body Wash</label></p>
@@ -27,24 +34,82 @@
                     <p id="sanitary"><input type = "checkbox"><label>Sanitary</label></p>
                       <p id="sanitary-price">Price: </p><input type="text" id="sanitary-pr"> 
                   </div>
-
                   <button type="submit" id="reg-ser">Submit</button>
                 </form>
+                
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
   data () {
     return{
-      services : false
+      service : false,
+      time: false,
+      data : {
+        grooming : {
+          opentime : {
+            weekdays : '',
+            weekends : ''
+          },
+          description : '',
+          groomingService : {
+            Dogs : [{
+                service : '',
+                price : '',
+            }],
+            Cats : [{
+                service : '',
+                price : '',
+            }],
+          },
+          additionalServices : '',
+        }
+      }
     }
   },
   methods : {
     showServices() {
-      this.services = !this.services
+      this.service = !this.service
+      this.time = false
+    },
+    showTime() {
+      this.time = !this.time
+      this.service = false
+    },
+    ...mapActions(["updateServices"]),
+    handleSubmit(){
+      console.log(JSON.parse(JSON.stringify(this.data)))
+      //this.updateServices(JSON.parse(JSON.stringify(this.data)))
+    }
+  },
+  computed : {
+    ...mapGetters(["services"]),
+    grooming () {
+      if (!this.services[0]){
+        return {
+          opentime : {
+            weekdays : '',
+            weekends : ''
+        },
+        description : '',
+        groomingService : {
+            Dogs : [{
+                service : '',
+                price : '',
+            }],
+            Cats : [{
+                service : '',
+                price : '',
+            }],
+        },
+        additionalServices : '',
+        }
+      }
+      return this.services[0].grooming
     }
   }
 }
@@ -107,6 +172,72 @@ export default {
   color: #707070;
   text-indent: 30px;
 }
+.time{
+  position: absolute;
+  top:60px;
+  left:5px;
+  width: 500px;
+  height:100px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border: 1px solid #DCDCDC;
+  border-radius: 3px;
+  opacity: 1;
+  z-index:100;
+}
+#weekdays{
+  position: absolute;
+  top:5px;
+  left:10px;
+  text-align: left;
+  font: normal normal 300 18px Poppins;
+  letter-spacing: 0px;
+  color: #707070;
+  opacity: 1;
+  text-indent: 10px;
+}
+#weekends{
+  position: absolute;
+  top:50px;
+  left:10px;
+  text-align: left;
+  font: normal normal 300 18px Poppins;
+  letter-spacing: 0px;
+  color: #707070;
+  opacity: 1;
+  text-indent: 10px;
+}
+#weekdaytime{
+  position: absolute;
+  top:5px;
+  left:200px;
+  width: 250px;
+  height: 35px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border: 1px solid #DCDCDC;
+  border-radius: 3px;
+  opacity: 1;
+  text-align: left;
+  font: normal normal 300 20px Poppins;
+  letter-spacing: 0px;
+  color: #707070;
+  text-indent: 30px;
+}
+#weekendtime{
+  position: absolute;
+  top:50px;
+  left:200px;
+  width: 250px;
+  height: 35px;
+  background: #FFFFFF 0% 0% no-repeat padding-box;
+  border: 1px solid #DCDCDC;
+  border-radius: 3px;
+  opacity: 1;
+  text-align: left;
+  font: normal normal 300 20px Poppins;
+  letter-spacing: 0px;
+  color: #707070;
+  text-indent: 30px;
+}
 #description{
   position: absolute;
   top:10px;
@@ -145,22 +276,6 @@ export default {
   left:250px;
   
 }
-#price{
-  position: absolute;
-  top:110px;
-  left:580px;
-  width: 500px;
-  height: 50px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  border: 1px solid #DCDCDC;
-  border-radius: 3px;
-  opacity: 1;
-  text-align: left;
-  font: normal normal 300 20px Poppins;
-  letter-spacing: 0px;
-  color: #707070;
-  text-indent: 30px;
-}
 #service{
   position: absolute;
   top:210px;
@@ -179,7 +294,7 @@ export default {
 }
 #other{
   position: absolute;
-  top:210px;
+  top:110px;
   left:580px;
   width: 500px;
   height: 50px;
@@ -192,6 +307,7 @@ export default {
   letter-spacing: 0px;
   color: #707070;
   text-indent: 30px;
+
 }
 .services{
   position: absolute;
@@ -453,5 +569,15 @@ export default {
   font: normal normal normal 20px/30px Poppins;
   letter-spacing: 0px;
   color: #FFFFFF;
+}
+#tab {
+  position: absolute;
+  top: 10px;
+  font: normal normal 300 15px Poppins;
+  padding: 10px;
+  letter-spacing: 0px;
+  color: #707070;
+  opacity: 1;
+  width : 1000px;
 }
 </style>
