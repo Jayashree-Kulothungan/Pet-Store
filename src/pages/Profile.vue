cc19cad437e80b58a2c2184d4afedc5b71a47c1d
Jayashree-Kulothungan <52423488+Jayashree-Kulothungan@users.noreply.github.com>
2020-12-21 09:03:35 +0530

    Update Profile.vue

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,30 +1,33 @@
 <template>
-<div>
-  <div class="profile">
-  <v-card>
-    <v-tabs vertical>
-      <v-tab id="my-acc" selected @click="showAccount" >  My Account  </v-tab>
-      <p id="services"  @click="showServices">Services<b-icon icon="chevron-down" id="arrow" scale="1"></b-icon></p>
-        <v-tab id="day-care" v-if="showStabs" @click="showDaycare">  Day Care  </v-tab>
-        <v-tab id="grooming" v-if="showStabs" @click="showGrooming">  Grooming  </v-tab>
-        <v-tab id="dog-walker" v-show="showStabs" @click="showDogwalker"> Dog Walker </v-tab>
-        <v-tab id="trainer" v-show="showStabs" @click="showTrainer"> Trainer </v-tab>
-        <v-tab id="clinics" v-show="showStabs" @click="showClinics">  Clinics  </v-tab>
-        <v-tab id="breeding" v-show="showStabs" @click="showBreeding">  Breeding </v-tab> 
-        <v-tab id="sell" v-show="showStabs">Sell</v-tab> 
-    </v-tabs>
-  </v-card>
-    <my-account v-if="myaccount"></my-account> 
-    <day-care v-if="daycare"></day-care>
-    <grooming v-if="grooming"></grooming>
-    <dog-walker v-if="dogwalker"></dog-walker>
-    <trainer v-if="trainer"></trainer>
-    <clinics v-if="clinics"></clinics>
-    <breeding v-if="breeding"></breeding>
-
-    
-</div>
-</div>
+    <div>
+        <div class="profile">
+            <div class="tabs"> 
+                  <v-card>
+                    <v-tabs vertical>
+                        <v-tab id="myreq" @click="showRequests"> My Requests </v-tab>
+                        <v-tab id="res" @click="showResponse"> Response </v-tab>
+                        <v-tab id="my-acc"  @click="showAccount" >  My Account  </v-tab>
+                        <p id="services"  @click="showServices">Services<b-icon icon="chevron-down" id="arrow" scale="1"></b-icon></p>
+                        <v-tab id="day-care" v-if="showStabs" @click="showDaycare">  Day Care  </v-tab>
+                        <v-tab id="grooming" v-if="showStabs" @click="showGrooming">  Grooming  </v-tab>
+                        <v-tab id="dog-walker" v-show="showStabs" @click="showDogwalker"> Dog Walker </v-tab>
+                        <v-tab id="trainer" v-show="showStabs" @click="showTrainer"> Trainer </v-tab>
+                        <v-tab id="clinics" v-show="showStabs" @click="showClinics">  Clinics  </v-tab>
+                        <v-tab id="breeding" v-show="showStabs" @click="showBreeding">  Breeding </v-tab>    
+                    </v-tabs>                
+                 </v-card>
+            </div>
+            <my-account v-if="myaccount"></my-account> 
+            <day-care v-if="daycare"></day-care>
+            <grooming v-if="grooming"></grooming>
+            <dog-walker v-if="dogwalker"></dog-walker>
+            <trainer v-if="trainer"></trainer>
+            <clinics v-if="clinics"></clinics>
+            <breeding v-if="breeding"></breeding>  
+            <my-requests v-if="myRequests"></my-requests>   
+            <response v-if="response"></response>       
+        </div>
+    </div>
 </template>
 
 <script>
@@ -35,11 +38,13 @@ import dogWalker from '../components/services/dogwalker'
 import Trainer from '../components/services/training'
 import Clinics from '../components/services/clinics'
 import Breeding from '../components/services/breeding'
+import myRequests from '../components/services/myRequests'
+import Response from '../components/services/response'
 import { mapActions, mapGetters } from "vuex";
 export default {
    data() {
     return {
-      myaccount: true,
+      myaccount: false,
       showStabs: false,
       daycare:false,
       grooming:false,
@@ -49,6 +54,8 @@ export default {
       breeding:false,
       regService :'',
       name : '',
+      myRequests:true,
+      response:false
       
     }
   },
@@ -63,6 +70,8 @@ export default {
       this.trainer=false;
       this.clinics=false;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showServices(){
       this.showStabs=!this.showStabs;
@@ -73,6 +82,8 @@ export default {
       this.trainer=false;
       this.clinics=false;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showDaycare(){
       this.myaccount=false;
@@ -82,6 +93,8 @@ export default {
       this.trainer=false;
       this.clinics=false;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showGrooming(){
       this.myaccount=false;
@@ -91,6 +104,8 @@ export default {
       this.trainer=false;
       this.clinics=false;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showDogwalker(){
       this.myaccount=false;
@@ -100,6 +115,8 @@ export default {
       this.trainer=false;
       this.clinics=false;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showTrainer(){
       this.myaccount=false;
@@ -109,6 +126,8 @@ export default {
       this.trainer=true;
       this.clinics=false;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showClinics(){
       this.myaccount=false;
@@ -118,6 +137,8 @@ export default {
       this.trainer=false;
       this.clinics=true;
       this.breeding=false;
+      this.myRequests=false;
+      this.response =false;
     },
     showBreeding(){
       this.myaccount=false;
@@ -127,6 +148,30 @@ export default {
       this.trainer=false;
       this.clinics=false;
       this.breeding=true;
+      this.myRequests=false;
+      this.response =false;
+    },
+    showRequests(){
+        this.myaccount=false;
+        this.daycare=false;
+        this.grooming=false;
+        this.dogwalker=false;
+        this.trainer=false;
+        this.clinics=false;
+        this.breeding=false;
+        this.myRequests=true;
+        this.response =false;
+    },
+    showResponse(){
+        this.myaccount=false;
+        this.daycare=false;
+        this.grooming=false;
+        this.dogwalker=false;
+        this.trainer=false;
+        this.clinics=false;
+        this.breeding=false;
+        this.myRequests=false;
+        this.response =true;
     }
   },
   created() {
@@ -139,15 +184,16 @@ export default {
     dogWalker,
     Trainer,
     Clinics,
-    Breeding
+    Breeding,
+    myRequests,
+    Response
   },
   
 };
 </script>
 
 <style scoped>
-
-  *:focus {
+ *:focus {
       outline: 0 !important;
   }
   .profile{
@@ -156,41 +202,6 @@ export default {
     left:0px;
   }
   #my-acc{
-    position: absolute;
-    top:0px;
-    left:0px;
-    width: 300px;
-    height: 70px;
-    background: #EBEBEB 0% 0% no-repeat padding-box;
-    border-radius: 3px;
-    opacity: 1;
-    font: normal normal normal 20px Poppins;
-    text-indent: 50px;
-    letter-spacing: 0px;
-    color: #707070!important;
-    border:none;
-  }
-  #services{
-    position: absolute;
-    top:70px;
-    left:0px;
-    width: 300px;
-    height: 70px;
-    background: #EBEBEB 0% 0% no-repeat padding-box;
-    border-radius: 3px;
-    opacity: 1;
-    font: normal normal normal 20px Poppins;
-    text-indent: 50px;
-    letter-spacing: 0px;
-    color: #707070!important;
-    border:none;
-  }
-  #arrow{
-    position: absolute;
-    left:90%;
-    
-  }
-  #day-care{
     position: absolute;
     top:140px;
     left:0px;
@@ -204,8 +215,9 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
   }
-  #grooming{
+  #services{
     position: absolute;
     top:210px;
     left:0px;
@@ -219,8 +231,15 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
   }
-  #dog-walker{
+  #arrow{
+    position: absolute;
+    left:50%;
+    top:10%
+    
+  }
+  #day-care{
     position: absolute;
     top:280px;
     left:0px;
@@ -234,8 +253,9 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
   }
-  #trainer{
+  #grooming{
     position: absolute;
     top:350px;
     left:0px;
@@ -249,8 +269,9 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
   }
-  #clinics{
+  #dog-walker{
     position: absolute;
     top:420px;
     left:0px;
@@ -264,8 +285,9 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
   }
-  #breeding{
+  #trainer{
     position: absolute;
     top:490px;
     left:0px;
@@ -279,8 +301,9 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
   }
-  #sell{
+  #clinics{
     position: absolute;
     top:560px;
     left:0px;
@@ -294,5 +317,54 @@ export default {
     letter-spacing: 0px;
     color: #707070;
     border:none;
+    cursor: pointer;
+  }
+  #breeding{
+    position: absolute;
+    top:630px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070;
+    border:none;
+    cursor: pointer;
+  } 
+  #myreq{
+    position: absolute;
+    top:0px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070!important;
+    border:none;
+    cursor: pointer;
+  }
+  #res{
+    position: absolute;
+    top:70px;
+    left:0px;
+    width: 300px;
+    height: 70px;
+    background: #EBEBEB 0% 0% no-repeat padding-box;
+    border-radius: 3px;
+    opacity: 1;
+    font: normal normal normal 20px Poppins;
+    text-indent: 50px;
+    letter-spacing: 0px;
+    color: #707070!important;
+    border:none; 
+    cursor: pointer;
   }
 </style>
